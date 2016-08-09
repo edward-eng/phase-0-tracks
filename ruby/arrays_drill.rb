527dbbe601a711578b79fc0fa2cd208b0d9dc331
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-09 18:22:21 -0500

    Explore array docs and initialize arrays

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,22 @@
+empty_array = []
+p empty_array
+
+empty_array << "item1" << "item2" << "item3" << "item4" << "item5"
+p empty_array
+
+empty_array.delete_at(2)
+p empty_array
+
+empty_array.insert(2, "new_item")
+p empty_array
+
+empty_array.shift
+p empty_array
+
+empty_array.include?("item5")
+p "Is it true or false that the array includes item5? #{empty_array.include?("item5")}"
+
+new_array = [1, 2, 3]
+
+combined_array = empty_array + new_array
+p combined_array
