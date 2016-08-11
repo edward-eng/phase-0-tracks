0da562b4c3caf12f940cf5487deb636f5f712bb0
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-11 18:38:46 -0500

    Pseudocode program

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+# Client Database
+
+# Ask the user to fill out the form as completely as possible.
+# Convert the inputs to the correct data types.
+# Store the inputs into variables.
+# Print the results to the console so the user can see what he or she has filled in.
+# Ask the user if he or she wants to update the values of any keys.
+# If the user wants to make any updates, ask the user which keys he or she would like to update. If the user types 'none', skip this step and print the results to the console.
+# Otherwise, update the values requested.
+# Print the result to the console.
