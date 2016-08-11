d96ca913c1b88dedbf9d6da3e733d8d4820d6891
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-10 20:48:52 -0500

    Use different methods to iterate through array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,15 +1,15 @@
-# # Release 0
+# Release 0
 
-# def add_numbers
-# 	print "Enter integer 1: "
-# 	num1 = gets.chomp.to_i
+def add_numbers
+	print "Enter integer 1: "
+	num1 = gets.chomp.to_i
 
-# 	print "Enter integer 2: "
-# 	num2 = gets.chomp.to_i
-# 	yield(num1, num2)
-# end
+	print "Enter integer 2: "
+	num2 = gets.chomp.to_i
+	yield(num1, num2)
+end
 
-# add_numbers {|int1, int2| puts "#{int1} + #{int2} = #{int1 + int2}"}
+add_numbers {|int1, int2| puts "#{int1} + #{int2} = #{int1 + int2}"}
 
 # Release 1
 
@@ -43,3 +43,40 @@ student_profile.each do |key, value|
 	puts "key: #{key}\n"\
 		 "value: #{value}\n\n"
 end
+
+# Release 2
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+favorite_food = {jeff: "pizza", edward: "crab", mary: "avocado"}
+
+# Do the Thing 1
+
+numbers.delete_if { |x| x < 5 }
+p numbers
+
+favorite_food.delete_if { |key, value| value == "avocado"} 
+p favorite_food
+
+# Do the Thing 2
+
+numbers = numbers.select { |num| num == 3 || num == 5 || num == 6 }
+p numbers
+
+favorite_food = favorite_food.select { |key, value| value == "pizza"}
+p favorite_food
+
+# Do the Thing 3
+
+numbers.keep_if { |x| x > 5}
+p numbers
+
+favorite_food = favorite_food.keep_if { |key, value| value == "crab"}
+p favorite_food
+
+# Do the Thing 4
+
+numbers = numbers.take_while { |x| x < 5}
+p numbers
+
+favorite_food = favorite_food.reject { |key, value| key == :mary }
+p favorite_food
