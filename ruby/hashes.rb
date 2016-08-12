52593bb99496cf9eaa41529da5210eccee2c1679
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-11 22:50:59 -0500

    Pseudocode and write client database program

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,6 +2,8 @@
 
 # Ask the user to fill in the information as completely as possible.
 # Convert the inputs to the correct data types.
+# Store the inputs into variables.
+# Print the results to the console so the user can see what he or she has filled in.
 
 puts "Hi, please answer the following questions."
 
@@ -17,9 +19,32 @@ decor_theme = gets.chomp
 puts "Does your client live in the city?"
 residence = gets.chomp
 
-# Store the inputs into variables.
-# Print the results to the console so the user can see what he or she has filled in.
-# Ask the user if he or she wants to update the values of any keys.
-# If the user wants to make any updates, ask the user which keys he or she would like to update. If the user types 'none', skip this step and print the results to the console.
-# Otherwise, update the values requested.
-# Print the result to the console.
+client_info = {name: name, age: age, decor: decor_theme, city: residence}
+p client_info
+
+# Ask user what changes he or she wants to make. (key or none)
+# Store the user's response (string) in user_response.
+# If 'none'
+# - Do nothing.
+# Else
+# - Change user's response into a symbol.
+# - Ask the user what he or she would like to change the value to.
+# - Store the change in a new variable called new_value.
+# - Reassign new_value to hash.
+# Print result.
+
+puts "What would you like to change? (name, age, decor, city, or none)"
+user_response = gets.chomp
+
+if user_response != "none"
+	user_response = user_response.to_sym
+	puts "What would you like to change?"
+	if user_response == :age
+		new_value = gets.chomp.to_i
+	else
+		new_value = gets.chomp
+	end
+	client_info[user_response] = new_value
+end
+
+p client_info
