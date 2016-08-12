d74e76ae72dea8e523f3c3931de8032f327ae804
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-12 18:00:52 -0500

    Build nested data structure to represent magic: the gathering bookbag

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+# Magic: The Gathering Bookbag
+
+mtg_bookbag = {
+	water_bottle: ['water']
+	deckbox: {
+		deck_name: 'Bant Coco',
+		contents: {
+			main_plus_side: 75,
+			key_main_cards: ['Collected Company', 'Spell Queller', 'Reflector Mage', 'Noose Constrictor'],
+			key_side_cards: ['Negate', 'Reality Smasher']
+		}
+	}
+	binder: {
+		binder_name: 'The Goods',
+		contents: {
+			blue_cards: 38,
+			lands: 18,
+			artifacts: 8
+			power_trades: ['Shocks', 'Volcanic Island', 'Tolarian Academy']
+			quick_dumps: ['Fiery Temper']
+		}
+	}
+	battle_gear: ['pen', 'paper', 'dice', 'playmat', 'extra sleeves']
+}
