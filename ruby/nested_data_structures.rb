770e20e843bcf4f04f5c4e767778b8b2a9a96d0e
Vanessa Chiang <edwardalaneng@gmail.com>
2016-08-12 19:43:36 -0500

    Print some nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,7 +1,7 @@
 # Magic: The Gathering Bookbag
 
 mtg_bookbag = {
-	water_bottle: ['water']
+	water_bottle: ['water'],
 	deckbox: {
 		deck_name: 'Bant Coco',
 		contents: {
@@ -9,16 +9,20 @@ mtg_bookbag = {
 			key_main_cards: ['Collected Company', 'Spell Queller', 'Reflector Mage', 'Noose Constrictor'],
 			key_side_cards: ['Negate', 'Reality Smasher']
 		}
-	}
+	},
 	binder: {
 		binder_name: 'The Goods',
 		contents: {
 			blue_cards: 38,
 			lands: 18,
-			artifacts: 8
-			power_trades: ['Shocks', 'Volcanic Island', 'Tolarian Academy']
+			artifacts: 8,
+			power_trades: ['Shocklands', 'Volcanic Island', 'Tolarian Academy'],
 			quick_dumps: ['Fiery Temper']
 		}
-	}
+	},
 	battle_gear: ['pen', 'paper', 'dice', 'playmat', 'extra sleeves']
 }
+
+p mtg_bookbag[:deckbox][:deck_name]
+p mtg_bookbag[:battle_gear][3]
+# p mtg_bookbag[:contents][:blue_cards]
