8442a22a01205c90cadd0574499356ba65c3941b
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-18 17:48:08 -0500

    Pseudocode dragon class

---
+++ ruby/solo.rb
@@ -0,0 +1,13 @@
+SPECIES ------------------------
+fire-breathing dragon
+
+CHARACTERISTICS ----------------
+Friendly: no
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+breathe fire
+hibernate
+fly
