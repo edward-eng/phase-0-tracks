c555a5c5f7eef8da6b0a7487d87453f9b7fc4086
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-17 20:54:07 -0500

    Update santa class with attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,18 @@
 class Santa
-	def speak(string)
-		puts string
-	end
-
-	def eat_milk_and_cookies(string)
-		puts "That was a good #{string}!"
-	end
-
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+	def speak(holiday_greeting)
+		puts holiday_greeting
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
 	end
 end
 
