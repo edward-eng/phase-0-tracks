14cf6494b707fd22a1b1db8f8730358e11b2bfcc
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-17 20:31:45 -0500

    Define santa class and add three instance methods

---
+++ ruby/santa.rb
@@ -0,0 +1,13 @@
+class Santa
+	def speak(string)
+		puts string
+	end
+
+	def eat_milk_and_cookies(string)
+		puts "That was a good #{string}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance ..."
+	end
+end
