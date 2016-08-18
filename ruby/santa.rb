633ea0d22c732016d9f879ffca9e4f60ba4dc9f8
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-18 15:35:14 -0500

    Add driver code to test methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -12,6 +12,7 @@ class Santa
 		puts "Santa Name: #{@name}"
 		puts "#{@name} is #{@gender}."
 		puts "#{@name} is #{@ethnicity}."
+		puts "#{@name}'s reindeer ranking is #{@reindeer_ranking}."
 		puts "#{@name} is #{@age} years old."
 	end
 
@@ -20,11 +21,53 @@ class Santa
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		puts "And #{@name} says, thanks for the #{cookie_type}!"
+		puts "And #{@name} says thanks for the #{cookie_type}!"
+	end
+
+	def celebrate_birthday(age)
+		@age + 1
+		puts "#{@name} just had a birthday and is #{age} now."
+	end
+
+	def get_mad_at(reindeer_name)
+		new_reindeer_ranking = @reindeer_ranking.rotate(1)
+		puts "#{name} got made at #{reindeer_name} and the new reindeer ranking is...#{new_reindeer_ranking}"
+	end
+
+	# getter methods for attributes
+	def name
+		@name
+	end
+
+	def gender
+		@gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	# setter methods for attributes
+	def name=(new_name)
+		@name = new_name
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
 	end
 end
 
 king_santa = Santa.new("Joe", "male", "Cantonese")
 king_santa.about
+puts "#{king_santa.name} is a baby."
+puts "#{king_santa.name} is #{king_santa.age} old and is #{king_santa.ethnicity}."
 king_santa.speak("Ho, ho, ho! Haaaappy holidays!" )
 king_santa.eat_milk_and_cookies("snickerdoodle")
+king_santa.name = "Happy Joe"
+puts "#{king_santa.name} says I'll be back next year!"
+king_santa.celebrate_birthday(1)
+king_santa.get_mad_at("Rudolph")
