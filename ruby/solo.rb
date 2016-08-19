799f029b8748f7c48c7742201c811b943338d758
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-18 19:18:56 -0500

    Write class and test methods with driver code

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,13 +1,43 @@
-SPECIES ------------------------
-fire-breathing dragon
+# SPECIES ------------------------
+# fire-breathing dragon
 
-CHARACTERISTICS ----------------
-Friendly: no
-Eye count: 2
-Name: varies
-Color: varies
+# CHARACTERISTICS ----------------
+# Friendly: no
+# Eye count: 2
+# Name: varies
+# Color: varies
 
-BEHAVIOR -----------------------
-breathe fire
-hibernate
-fly
+# BEHAVIOR -----------------------
+# breathe fire
+# hibernate
+# fly
+
+class Dragon
+	attr_reader :eye_count, :friendliness
+	attr_accessor :name, :color
+
+	def initialize(name, color)
+		puts "Initializing dragon instance..."
+		@name = name
+		@color = color
+		@eye_count = 2
+		@friendliness = false
+	end
+
+	def breathe_fire
+		puts "WHOOOOOOMPH! *sizzle*"
+	end
+
+	def hibernate(hours)
+		puts "#{@name} sleeps for #{hours} hours."
+	end
+
+	def fly(length)
+		puts "#{@name} flies #{length} feet high."
+	end
+end
+
+fire_breathing_dragon = Dragon.new("Kevin", "Green")
+fire_breathing_dragon.breathe_fire
+fire_breathing_dragon.hibernate(10)
+fire_breathing_dragon.fly(10000)
