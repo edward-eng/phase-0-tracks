9c603c4f2957d14019e00b40114bec9e27465153
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-19 16:19:22 -0500

    Add bear and pirate classes. Add driver code to test shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -11,3 +11,22 @@
 # puts Shout.yell_angrily("Nooooo")
 # puts Shout.yelling_happily("Ahhhhh")
 
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+end
+
+class Bear
+	include Shout
+end
+
+class Pirate
+	include Shout
+end
+
+smokey = Bear.new
+smokey.yell_angrily("FIRE")
+
+jack = Pirate.new
+jack.yelling_happily("WHERE'S MY BEER")
