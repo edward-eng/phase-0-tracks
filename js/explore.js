3a23217caa5b6aaf4b4eccbde252be72ba613700
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-23 20:51:08 -0500

    Add reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,12 @@
 // Write a function that takes a string as a parameter.
-// Reverse the string. Take the first letter and move it to the back until the last letter is the first letter.
+// Reverse the string by taking the first letter and moving it to the back until the last letter is the first letter.
+
+function reverse_string(string){
+	var new_string = string;
+	var array = new_string.split("");
+	var reverse_array = array.reverse();
+	var result = reverse_array.join("");
+	console.log(result);
+}
+
+reverse_string("hello");
