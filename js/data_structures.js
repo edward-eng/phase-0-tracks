b0b2cb5358d9ea04cb9ffed73c29df0ee0fb7194
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-24 18:57:26 -0500

    Update the pseudocode and conditional statement to data_structures.js to include how many items to add to the arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,13 +14,23 @@ console.log(names);
 // Check to see if the colors and names arrays are the same length.
 // If they are the same length, then loop through both arrays and pair the indexes as key and value.
 // Then print the hash to the console.
-// If they're not the same length, then determine which array is shorter and alert the user to input another item in the array.
+// If they're not the same length, then determine which array is shorter and alert the user to input that many more item(s) in the array.
 
 var stable = {};
+names.push("Joe");
+console.log(names);
 
 if (colors.length == names.length){
 	for (var i = 0; i < colors.length; i++){
 		stable[names[i]] = colors[i];
 	}
 	console.log(stable);
+} else {
+	if (colors.length > names.length) {
+		var extra_colors = colors.length - names.length;
+		console.log("You need to enter " + extra_colors + " more name(s).");
+	} else {
+		var extra_names = names.length - colors.length;
+		console.log("You need to enter " + extra_names + " more color(s).");
+	}
 }
