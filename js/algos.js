fd56a1f7e61a2ff7a7eb8b6381f09c11b665e86d
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-28 18:00:39 -0500

    Pseudocode longest_string function.

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// Pseudocode (longest_string function)
+// Add a longest_string function.
+// The function takes in an array (of words or phrases) as a parameter.
+// Then the function iterates through the array to find the longest word or phrase.
+// Then the function returns that word or phrase.
