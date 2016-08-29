abf15ca64b3aef324dca3266b431df4592cff98f
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-28 20:04:02 -0500

    Pseudocode key_value_match function.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,5 @@
 // Pseudocode (longest_string function)
+
 // Add a longest_string function.
 // The function takes in an array (of words or phrases) as a parameter.
 // Then the function iterates through the array to see how long each string is.
@@ -9,7 +10,7 @@ function longest_string(array){
 	for (var i = 0; i < array.length; i++){
 		// If the length of the current element in the array is longer than the length of the longest_string_found variable:
 		// Store the current element in the longest_string_found variable.
-		// Return the longest_string_found.
+		// Then return the longest_string_found.
 		if (array[i].length > longest_string_found.length){
 			longest_string_found = array[i]
 		}
@@ -19,3 +20,13 @@ function longest_string(array){
 
 var result = longest_string(['cat', 'dinosaur', 'mouse']);
 console.log(result)
+
+// Pseudocode (key_value_match function)
+
+// Add key_value_match function.
+// The function takes in two objects.
+// The function iterates through the hash to look at the keys and values of each object.
+// If the keys or the values of both objects match:
+// The function should return <true>.
+// Otherwise (else), the function should return <false>.
+
