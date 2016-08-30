0a66a0695e41d7c559e91efc1d46a4618de88577
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-29 20:49:03 -0500

    Update random_string function in algos.js file.

--- js/algos.js
+++ js/algos.js
@@ -83,7 +83,7 @@ console.log(random_test_data(3))
 // The function should determine how long the random string should be by randomly choosing a number from 1 through 10.
 // Then the function should look through the alphabet and randomly choose letters until the number of letters equals the number chosen.
 
-function random_string {
-	var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+function random_string{
+	var random_number = Math.floor((Math.random() * 10) + 1);
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
 }
