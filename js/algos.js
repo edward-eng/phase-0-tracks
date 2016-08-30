f516ac23e2efc08a5f5a6858fa1d2b05c6bc855d
Vanessa Chiang <vcchiang@Vanessa-Chiangs-MacBook-Air.local>
2016-08-29 22:18:16 -0500

    Update key_value_match function to return correct true/false output. Add random_word_length, random_letter, and build_random_word functions. Update random_test_data function to call those functions to generate array of 3 random strings of 1 through 10 letters each.

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,4 @@
 // Pseudocode (longest_string function)
-
 // Add a longest_string function.
 // The function takes in an array (of words or phrases) as a parameter.
 // Then the function iterates through the array to see how long each string is.
@@ -22,7 +21,6 @@ var result = longest_string(['cat', 'dinosaur', 'mouse']);
 console.log(result)
 
 // Pseudocode (key_value_match function)
-
 // Add key_value_match function.
 // The function takes in two objects.
 // The function iterates through each object to look at the keys and values.
@@ -33,20 +31,27 @@ console.log(result)
 function key_value_match(object1, object2){
 	var match = false;
 	for (var info1 in object1){
-		if (object1.hasOwnProperty(info1)){
 			console.log(info1 + ": " + object1[info1]);
-		}
 	}
 	for (var info2 in object2){
-		if (object2.hasOwnProperty(info2)){
 			console.log(info2 + ": " + object2[info2]);
+	}
+
+	for (var info1 in object1){
+		if (object2.hasOwnProperty(info1)){
+			if (object1[info1] == object2[info1]){
+				return match = true;
+			}
+			else if (object1[info2] == object2[info2]){
+				return match = true;
+			}
+			else {
+				return match = false;
+			}
 		}
-	}
-	if (object1.hasOwnProperty(info1) == object2.hasOwnProperty(info2)){
-		return match = true;
-	}
-	else {
-		return match = false;
+		else {
+			return match = false;
+		}
 	}
 }
 
@@ -54,22 +59,20 @@ function key_value_match(object1, object2){
 var match_result = key_value_match({name: "Steven", age: 54}, {name: "Tamir", age: 54})
 console.log(match_result)
 
-// Not sure why this still returns <true>.
 var match_result = key_value_match({name: "Steven", age: 54}, {name: "Tamir", age: 14})
 console.log(match_result)
 
 // Pseudocode (random_test_data function)
-
 // Add random_test_data function.
 // The function should take an integer to determine the length an of array.
 // Then the function should build an array.
-// Then the function should populate the array with random strings until the number of strings equals the integer given (random_string()).
+// Then the function should populate the array with random strings until the number of strings equals the integer given.
 // The random strings should contain at least 1 letter but not more than 10.
 
 function random_test_data(array_length){
 	empty_array = []
 	for (var i = 0; i < array_length; i++){
-		empty_array.push('a');
+		empty_array.push(build_random_word());
 	}
 	return(empty_array);
 }
@@ -77,13 +80,39 @@ function random_test_data(array_length){
 random_test_data(3)
 console.log(random_test_data(3))
 
-// Pseudocode (random_string function)
+// Pseudocode (random_word_length function)
+// Pick a random number from 1 through 10.
 
-// Add random_string function.
-// The function should determine how long the random string should be by randomly choosing a number from 1 through 10.
-// Then the function should look through the alphabet and randomly choose letters until the number of letters equals the number chosen.
-
-function random_string{
+function random_word_length() {
 	var random_number = Math.floor((Math.random() * 10) + 1);
+	return random_number;
+}
+
+// console.log(random_word_length());
+
+// Pseudocode (random_letter function)
+// Pick a random letter based a random index of the alphabet.
+
+function random_letter() {
+	var random_number = Math.floor((Math.random() * 25) + 1);
 	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	return alphabet[random_number];
+}
+
+// console.log(random_letter());
+
+// Pseudocode (build_random_word function)
+// Call random_word_length function to get a random number from 1 through 10.
+// Store that number in a variable called word_length.
+// Create a empty string and store it in a variable called random_string.
+// Run a loop to generate a random string that's the same length as the word_length.
+// Return the random_string.
+
+function build_random_word(){
+	var word_length = random_word_length();
+	var random_string = "";
+	for (var i = 0; i < word_length; i++){
+		random_string += random_letter();
+	}
+	return random_string;
 }
