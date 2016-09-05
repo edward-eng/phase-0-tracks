4d1e0c51e7c5cce6b758b349423f5ea9cf8bdbeb
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-05 13:12:30 -0500

    Pseudocde and write fibonacci method. Add driver codd to test method.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -7,23 +7,46 @@
 # Otherwise, return nil.
 # Increment the loop counter by 1.
 
-arr = [42, 89, 23, 1]
+# arr = [42, 89, 23, 1]
 
-def search_array(numbers, target)
-	index = 0
-	while index < numbers.length
-		if numbers[index] == target 
-			return index
-		else
-			nil
-		end
-		index += 1
+# def search_array(numbers, target)
+# 	index = 0
+# 	while index < numbers.length
+# 		if numbers[index] == target 
+# 			return index
+# 		else
+# 			nil
+# 		end
+# 		index += 1
+# 	end
+# end
+
+# p search_array(arr, 42)
+# p search_array(arr, 89)
+# p search_array(arr, 23)
+# p search_array(arr, 1)
+# p search_array(arr, 100)
+# p search_array(arr, 200)
+
+# Write method that takes one parameter.
+# The parameter should be how long the Fibonacci is.
+# Start out with an array of 0 and 1.
+# Add numbers to the array until the length of the array equals the number of the parameter.
+# Add the 0 and 1 and store the result in the array.
+# Check to see how many numbers are in the array.
+# If the amount of numbers in the array equals the number of the parameter, stop and return the array.
+# If the amount of numbers in the array is less than the number of the parameter add the last two numbers in the array and store that number in the array.
+# Check the array again to see how many numbers are in it.
+
+def fib(integer)
+	array = [0, 1]
+	until array.length == integer
+	array << array[-1] + array[-2]
 	end
+	p array
 end
 
-p search_array(arr, 42)
-p search_array(arr, 89)
-p search_array(arr, 23)
-p search_array(arr, 1)
-p search_array(arr, 100)
-p search_array(arr, 200)
+fib(100)
+
+# Output
+# The array should fill in according to the parameter based on our fibonacci alogrithm.
