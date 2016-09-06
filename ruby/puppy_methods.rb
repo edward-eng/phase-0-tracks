9bbd40cc7e8a440bb9f4ab6a157e8ee91c83680d
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-06 13:51:02 -0500

    Create a loop to make multiple instances of the fish class. Store the instances in an array. Iterate over the array and call the instance methods on each instance in puppy_methods.rb file.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,44 +1,44 @@
 # New Stuff
 
-class Puppy
+# class Puppy
 
-  def initialize
-    puts "Initializing new puppy instance ..."
-  end
+#   def initialize
+#     puts "Initializing new puppy instance ..."
+#   end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-  def speak(integer)
-    puts "Woof! " * integer
-  end
+#   def speak(integer)
+#     puts "Woof! " * integer
+#   end
 
-  def roll_over
-    puts "*rolls over*"
-  end
+#   def roll_over
+#     puts "*rolls over*"
+#   end
 
-  def dog_years(human_years)
-    human_years * 7
-  end
+#   def dog_years(human_years)
+#     human_years * 7
+#   end
 
-  def sleep(hours)
-    puts "I slept for #{hours} hours!"
-  end
+#   def sleep(hours)
+#     puts "I slept for #{hours} hours!"
+#   end
 
-end
+# end
 
-chihuahua = Puppy.new
-chihuahua.fetch('ball')
-chihuahua.speak(5)
-chihuahua.roll_over
-chihuahua.dog_years(4)
-chihuahua.sleep(15)
+# chihuahua = Puppy.new
+# chihuahua.fetch('ball')
+# chihuahua.speak(5)
+# chihuahua.roll_over
+# chihuahua.dog_years(4)
+# chihuahua.sleep(15)
 
 
 class Fish
-
+    
   def initialize
     puts "Initializing new fish instance...."
   end
@@ -52,11 +52,30 @@ class Fish
   end
 end
 
+fish_array = []
+p fish_array
+
+counter = 0
+
+# Created just 2 instances for testing purposes.
+until counter == 2
+  new_name = Fish.new
+  fish_array << new_name
+  counter += 1
+end
+
+p fish_array
+
+fish_array.each do |name|
+  new_name.swim(5)
+  new_name.eat('shrimp')
+end
 
 #Driver Code:
-nemo = Fish.new
-nemo.swim(8)
-nemo.eat('anchovies')
+# nemo = Fish.new
+# nemo.swim(8)
+# nemo.eat('anchovies')
+
 # ========================================
 
 # Old Stuff
