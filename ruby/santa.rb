14f4e341d9fa9d7415a0f879e66201a9d47f0612
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-06 17:10:41 -0500

    Add santa class. Add initialize, speak, and eat_milk_and_cookies instance methods. Add driver code to test instance methods in santa.rb file.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,72 +1,96 @@
 class Santa
-	attr_reader :age, :ethnicity
-	attr_accessor :name, :gender
 
-	def initialize(name, gender, ethnicity)
+	def initialize
 		puts "Initializing Santa instance ..."
-		@name = name
-		@gender = gender
-		@ethnicity = ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
 	end
 
-	def about
-		puts "Santa Name: #{@name}"
-		puts "#{@name} is #{@gender}."
-		puts "#{@name} is #{@ethnicity}."
-		puts "#{@name}'s reindeer ranking is #{@reindeer_ranking}."
-		puts "#{@name} is #{@age} years old."
-	end
-
-	def speak(holiday_greeting)
-		puts "#{@name} says, '#{holiday_greeting}'"
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
-		puts "And #{@name} says thanks for the #{cookie_type}!"
-	end
-
-	def celebrate_birthday
-		@age += 1
-		puts "#{@name} just had a birthday and is #{age} now."
-	end
-
-	def get_mad_at(reindeer_name)
-		new_reindeer_ranking = @reindeer_ranking.rotate(1)
-		puts "#{name} got mad at #{reindeer_name} and the new reindeer ranking is...#{new_reindeer_ranking}"
+		puts "That was a good #{cookie_type} cookie!"
 	end
 end
 
-king_santa = Santa.new("Joe", "male", "Brazillian")
-king_santa.about
-puts "#{king_santa.name} is a baby."
-puts "#{king_santa.name} is #{king_santa.age} old and is #{king_santa.ethnicity}."
-king_santa.speak("Ho, ho, ho! Haaaappy holidays!" )
-king_santa.eat_milk_and_cookies("snickerdoodle")
-king_santa.name = "Happy Joe"
-puts "#{king_santa.name} says I'll be back next year!"
-king_santa.celebrate_birthday
-king_santa.get_mad_at("Rudolph")
+# Drive Code
+mister_claus = Santa.new
+mister_claus.speak
+mister_claus.eat_milk_and_cookies('chocolate chip')
 
-# Make an empty container for all the santas we create.
-santas = []
+# ==============================
 
-# Make an array of names to choose from for each santa we create.
-names = ["Mary", "Kelly", "Jessica", "Bob", "Chris", "John"]
+# Old Work
 
-# Make an array of example genders to choose from for each santa we create.
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# class Santa
+# 	attr_reader :age, :ethnicity
+# 	attr_accessor :name, :gender
 
-# Make an arry of example ethnicities to choose from for each santa we create.
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# 	def initialize(name, gender, ethnicity)
+# 		puts "Initializing Santa instance ..."
+# 		@name = name
+# 		@gender = gender
+# 		@ethnicity = ethnicity
+# 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+# 		@age = 0
+# 	end
 
-i = 0
+# 	def about
+# 		puts "Santa Name: #{@name}"
+# 		puts "#{@name} is #{@gender}."
+# 		puts "#{@name} is #{@ethnicity}."
+# 		puts "#{@name}'s reindeer ranking is #{@reindeer_ranking}."
+# 		puts "#{@name} is #{@age} years old."
+# 	end
 
-until i == 25
-	age = rand(100)
-	puts "Creating santa named #{names.sample} whose gender is #{example_genders.sample}, ethnicity is #{example_ethnicities.sample}, and is #{age}."
-	# Choose a random number between 0 and 140.
-	# Set this number to the new santa's age.
-	i += 1
-end
+# 	def speak(holiday_greeting)
+# 		puts "#{@name} says, '#{holiday_greeting}'"
+# 	end
+
+# 	def eat_milk_and_cookies(cookie_type)
+# 		puts "And #{@name} says thanks for the #{cookie_type}!"
+# 	end
+
+# 	def celebrate_birthday
+# 		@age += 1
+# 		puts "#{@name} just had a birthday and is #{age} now."
+# 	end
+
+# 	def get_mad_at(reindeer_name)
+# 		new_reindeer_ranking = @reindeer_ranking.rotate(1)
+# 		puts "#{name} got mad at #{reindeer_name} and the new reindeer ranking is...#{new_reindeer_ranking}"
+# 	end
+# end
+
+# king_santa = Santa.new("Joe", "male", "Brazillian")
+# king_santa.about
+# puts "#{king_santa.name} is a baby."
+# puts "#{king_santa.name} is #{king_santa.age} old and is #{king_santa.ethnicity}."
+# king_santa.speak("Ho, ho, ho! Haaaappy holidays!" )
+# king_santa.eat_milk_and_cookies("snickerdoodle")
+# king_santa.name = "Happy Joe"
+# puts "#{king_santa.name} says I'll be back next year!"
+# king_santa.celebrate_birthday
+# king_santa.get_mad_at("Rudolph")
+
+# # Make an empty container for all the santas we create.
+# santas = []
+
+# # Make an array of names to choose from for each santa we create.
+# names = ["Mary", "Kelly", "Jessica", "Bob", "Chris", "John"]
+
+# # Make an array of example genders to choose from for each santa we create.
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+# # Make an arry of example ethnicities to choose from for each santa we create.
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+# i = 0
+
+# until i == 25
+# 	age = rand(100)
+# 	puts "Creating santa named #{names.sample} whose gender is #{example_genders.sample}, ethnicity is #{example_ethnicities.sample}, and is #{age}."
+# 	# Choose a random number between 0 and 140.
+# 	# Set this number to the new santa's age.
+# 	i += 1
+# end
