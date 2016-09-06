32b345fa00cd8315682db7109278df79fc984184
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-05 21:56:33 -0500

    Add hash, store aliases in hash, and iterate through hash to print the data the user entered and the aliases in alias_manager.rb file.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,10 +10,20 @@
 # If the consonant is a 'z', then change it to an 'a'.
 
 # User Interface
+
+aliases = {}
+
 loop do
 	puts "Do you want to scramble a name? (yes or 'q' for quit)"
 	input = gets.chomp
-	break if input == 'q'
+	if input == 'q'
+		puts aliases
+		# Iterate through the hash
+		aliases.each do |real_name, result|
+			puts "#{real_name} is also known as #{result}."
+		end
+		break
+	end
 
 # Ask the user for his or her first name.
 puts "Please enter first name:"
@@ -27,7 +37,7 @@ puts "Please enter last name:"
 # Get the response and store it in a variable called <last_name>.
 last_name = gets.chomp
 
-def fake_name(real_name)
+def fake_name(real_name, aliases)
 
 	vowels = ['a', 'e', 'i', 'o', 'u']
 
@@ -59,9 +69,10 @@ def fake_name(real_name)
 		result = split_name.join('')
 		join_real_name = real_name.join(' ')
 		puts "#{join_real_name} is #{result}."
+		aliases[join_real_name] = result
 end
 
-fake_name(["#{first_name}", "#{last_name}"])
+fake_name(["#{first_name}", "#{last_name}"], aliases)
 
 end
 
@@ -73,74 +84,74 @@ end
 
 # User Interface
 
-loop do
-	puts "Do you want to scramble a name? (yes or 'q' for quit)"
-	input = gets.chomp
-	break if input == 'q'
+# loop do
+# 	puts "Do you want to scramble a name? (yes or 'q' for quit)"
+# 	input = gets.chomp
+# 	break if input == 'q'
 
-# Ask the user for his or her first name.
-puts "Please enter first name:"
+# # Ask the user for his or her first name.
+# puts "Please enter first name:"
 
-# Get the response and store it in a variable called <first_name>.
-first_name = gets.chomp
+# # Get the response and store it in a variable called <first_name>.
+# first_name = gets.chomp
 
-# Ask the user for his or her last name.
-puts "Please enter last name:"
+# # Ask the user for his or her last name.
+# puts "Please enter last name:"
 
-# Get the response and store it in a variable called <last_name>.
-last_name = gets.chomp
+# # Get the response and store it in a variable called <last_name>.
+# last_name = gets.chomp
 
-# Business Logic
+# # Business Logic
 
-# Add the first and last name variables together with a space between them and store the result in another variable called <full_name>.
-full_name = "#{[first_name, last_name].join(' ')}"
+# # Add the first and last name variables together with a space between them and store the result in another variable called <full_name>.
+# full_name = "#{[first_name, last_name].join(' ')}"
 
-# Break the <full_name> up into two strings and store the result in an array. Then store the array in a new variable called <split_name>.
-split_name = full_name.split
+# # Break the <full_name> up into two strings and store the result in an array. Then store the array in a new variable called <split_name>.
+# split_name = full_name.split
 
-# Take the <split_name> and swap the first and last names and store the result in a new variable called <swap_name>.
-swap_name = split_name.reverse
+# # Take the <split_name> and swap the first and last names and store the result in a new variable called <swap_name>.
+# swap_name = split_name.reverse
 
-# Take the <swap_name> and convert it back to a string with a space between the two names and store the result in a new variable called <new_string_name>.
-new_string_name = swap_name.join(' ')
+# # Take the <swap_name> and convert it back to a string with a space between the two names and store the result in a new variable called <new_string_name>.
+# new_string_name = swap_name.join(' ')
 
-# Take the <new_string_name> and break each name up into individual letters and store the result in an array. Then store that array in a new variable called <split_new_string_name>.
-split_new_string_name = new_string_name.split('')
+# # Take the <new_string_name> and break each name up into individual letters and store the result in an array. Then store that array in a new variable called <split_new_string_name>.
+# split_new_string_name = new_string_name.split('')
 
-# Make a variable called <vowels> and store the array of vowels 'a, e, i, o, u' in the variable.
-vowels = ['a', 'e', 'i', 'o', 'u']
+# # Make a variable called <vowels> and store the array of vowels 'a, e, i, o, u' in the variable.
+# vowels = ['a', 'e', 'i', 'o', 'u']
 
-# Make a variable called <consonants> and store the array of consonants 'b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z' in the variable.
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+# # Make a variable called <consonants> and store the array of consonants 'b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z' in the variable.
+# consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
-# Check to see whether each letter in <split_new_string_name> is a vowel or consonant.
-# If the letter is a vowel, change it to the next vowel.
-# If the letter is a consonant, change it to the next consonant.
-split_new_string_name.map! do |letters|
-	if vowels.include?(letters)
-		if letters == 'u'
-			letters = 'a'
-		else
-			vowels.index(letters)
-			vowels[vowels.index(letters) + 1]
-		end
-	elsif consonants.include?(letters)
-		if letters == 'z'
-			letters = 'b'
-		else
-			consonants.index(letters)
-			consonants[consonants.index(letters) + 1]
-		end
-	else
-		letters
-	end
-end
+# # Check to see whether each letter in <split_new_string_name> is a vowel or consonant.
+# # If the letter is a vowel, change it to the next vowel.
+# # If the letter is a consonant, change it to the next consonant.
+# split_new_string_name.map! do |letters|
+# 	if vowels.include?(letters)
+# 		if letters == 'u'
+# 			letters = 'a'
+# 		else
+# 			vowels.index(letters)
+# 			vowels[vowels.index(letters) + 1]
+# 		end
+# 	elsif consonants.include?(letters)
+# 		if letters == 'z'
+# 			letters = 'b'
+# 		else
+# 			consonants.index(letters)
+# 			consonants[consonants.index(letters) + 1]
+# 		end
+# 	else
+# 		letters
+# 	end
+# end
 
-# Print the result to the console.
-# p split_new_string_name
-fake_name = split_new_string_name.join('')
-# p fake_name
+# # Print the result to the console.
+# # p split_new_string_name
+# fake_name = split_new_string_name.join('')
+# # p fake_name
 
-puts "#{full_name} is also known as #{fake_name}."
+# puts "#{full_name} is also known as #{fake_name}."
 
-end
+# end
