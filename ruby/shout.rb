37acc064104d80a55c007412728b6f4d6741899b
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-07 17:15:40 -0500

    Add shout module. Add yell_angrily and yelling_happily methods to module. Add driver code to test module methods in shout.rb file.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,15 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	puts "#{words}! :)"
+  end
 end
+
+Shout.yell_angrily('WTF')
+Shout.yelling_happily('Yeehaw')
 
 
 
@@ -22,22 +31,22 @@ end
 # puts Shout.yell_angrily("Nooooo")
 # puts Shout.yelling_happily("Ahhhhh")
 
-module Shout
-  def yell_angrily(words)
-    words + "!!!" + " :("
-  end
-end
+# module Shout
+#   def yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+# end
 
-class Bear
-	include Shout
-end
+# class Bear
+# 	include Shout
+# end
 
-class Pirate
-	include Shout
-end
+# class Pirate
+# 	include Shout
+# end
 
-smokey = Bear.new
-puts smokey.yell_angrily("FIRE")
+# smokey = Bear.new
+# puts smokey.yell_angrily("FIRE")
 
-jack = Pirate.new
-puts jack.yell_angrily("WHERE'S MY BEER")
+# jack = Pirate.new
+# puts jack.yell_angrily("WHERE'S MY BEER")
