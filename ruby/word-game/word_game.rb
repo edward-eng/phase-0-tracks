ae5149c6db5a8f559a745be9b637712404382687
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-08 18:07:44 -0500

    Add game pseudocode to word_game.rb file. Reference word_game.rb file in word_game_spec.rb file.

---
+++ ruby/word-game/word_game.rb
@@ -0,0 +1,11 @@
+# Pseudocode
+# Input 1: a word to guess (string)
+# Input 2: a letter (string)
+# Output: The correctly guessed word (string) with a message saying 'you won!' or what the user guessed (string) with a message saying 'you failed!'
+# Create a game class (i.e. Word_Guesser)
+# Write a method to initialize the game
+# The game should ask for a word to guess upon initialization (argument/parameter)
+# Write a method to keep track of how many guesses the user has tried
+# Write a method that shows the user what he or she has guessed so far
+# Write a method that tells the user if he or she won or lost
+
