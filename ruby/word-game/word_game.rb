28b06542544a90a3a113a72eea8678c3666a82a8
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-08 23:07:54 -0500

    Update pseudocode. Add guesses_allowed_counter, store_letter, and hidden_word tests to word_game_spec.rb file. Add instance methods to word_game.rb file to pass spec tests in spec file.

--- ruby/word-game/word_game.rb
+++ ruby/word-game/word_game.rb
@@ -1,20 +1,40 @@
 # Pseudocode
-# Input 1: a word to guess (string)
-# Input 2: a letter (string)
-# Output: The correctly guessed word (string) with a message saying 'you won!' or what the user guessed (string) with a message saying 'you failed!'
-# Create a game class (i.e. Word_Guesser)
-# Write a method to initialize the game
-# The game should ask for a word to guess upon initialization (argument/parameter)
-# Write a method to keep track of how many guesses the user has tried
-# Write a method that shows the user what he or she has guessed so far
-# Write a method that tells the user if he or she won or lost
+
+# User_1 Input: a word
+# User_2 Input: a letter
+# Output: A message telling User_2 what the correctly guessed word is and a message saying 'you won!' OR a message telling User_2 what he or she guessed and a message saying 'you fail!'
+
+# Make a game class
+# Ask User_1 to type in a word
+# Let User_2 know that he or she has a number of guesses that's equal to twice the length of the word
+# Show User_2 a hidden word with underscores each representing a letter in the word and a space between each underscore
+# Ask User_2 to type in a letter
+# If the letter is in the word, replace the underscore with that letter
+# Show User_2 an updated version of the hidden word
+# Let User_2 know how many guesses he or she has left
+# If User_2 guesses all the letters correctly before he or she runs out of guesses, print a message showing the correctly guessed word and 'you win!'
+# Otherwise, print a message showing User_2 the word he or she guessed and 'you fail!' when User_2 runs out of guesses
 
 class Word_Guesser
-	def initialize(word_to_guess)
-		@guess_word = word_to_guess
+	def initialize(inputted_word, inputted_letter)
+		@guess_word = inputted_word
+		@letter = inputted_letter
 	end
 
 	def key_word
 		@guess_word
+	end
+
+	def guesses_allowed_counter
+		guesses_allowed = @guess_word.length
+		guesses_allowed
+	end
+
+	def store_letter
+		@letter
+	end
+
+	def hidden_word
+		"_ " * (@guess_word.length - 1) + "_"
 	end
 end
--- ruby/word-game/word_game_spec.rb
+++ ruby/word-game/word_game_spec.rb
@@ -1,9 +1,21 @@
 require_relative 'word_game'
 
 describe Word_Guesser do
-	let(:game) { Word_Guesser.new("word_to_guess") }
+	let(:game) { Word_Guesser.new("kaladesh", "z") }
 
-	it "stores the word_to_guess inputted by user upon initialization" do
-	expect(game.key_word).to eq "word_to_guess"
+	it "stores the word User_1 inputted upon initialization" do
+		expect(game.key_word).to eq "kaladesh"
+	end
+
+	it "keeps track of how many guesses User_2 has left" do
+		expect(game.guesses_allowed_counter).to eq 8
+	end
+
+	it "stores the letter User_2 inputted" do
+		expect(game.store_letter).to eq "z"
+	end
+
+	it "shows a hidden word with underscores each representing a letter in the word and spaces between each underscore" do
+		expect(game.hidden_word).to eq "_ _ _ _ _ _ _ _"
 	end
 end
