d356292a6dd670ce93d2dbc17638631dfd6ce922
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-09 02:08:03 -0500

    Add find_index test to word_game_spec.rb file. Add instance method to word_game.rb file to pass spec test.

--- ruby/word-game/word_game.rb
+++ ruby/word-game/word_game.rb
@@ -4,11 +4,11 @@
 # User_2 Input: a letter
 # Output: A message telling User_2 what the correctly guessed word is and a message saying 'you won!' OR a message telling User_2 what he or she guessed and a message saying 'you fail!'
 
-# Make a game class
-# Ask User_1 to type in a word
-# Let User_2 know that he or she has a number of guesses that's equal to twice the length of the word
-# Show User_2 a hidden word with underscores each representing a letter in the word and a space between each underscore
-# Ask User_2 to type in a letter
+# Make a game class (completed)
+# Ask User_1 to type in a word (implement in user interface)
+# Let User_2 know that he or she has a number of guesses that's equal to twice the length of the word (implement in user interface)
+# Show User_2 a hidden word with underscores each representing a letter in the word and a space between each underscore (implement in user interface)
+# Ask User_2 to type in a letter (implement in user interface)
 # If the letter is in the word, replace the underscore with that letter
 # Show User_2 an updated version of the hidden word
 # Let User_2 know how many guesses he or she has left
@@ -26,7 +26,7 @@ class Word_Guesser
 	end
 
 	def guesses_allowed_counter
-		guesses_allowed = @guess_word.length
+		guesses_allowed = @guess_word.length * 2
 		guesses_allowed
 	end
 
@@ -35,6 +35,32 @@ class Word_Guesser
 	end
 
 	def hidden_word
-		"_ " * (@guess_word.length - 1) + "_"
+		"_" * @guess_word.length
 	end
+
+	def find_index
+		guess_word_array = @guess_word.split('')
+		guess_word_array.each_index.select do |index|
+			guess_word_array[index] == @letter
+		end
+	end
+
+	# def show_letter
+	# 	p hidden_word_array = hidden_word.split('')
+	# 	hidden_word_array.map! do |underscore|
+	# 		if underscore == find_index
+	# 			underscore[find_index] = @letter
+	# 		end
+	# 	end
+	# 	p hidden_word_array
+	# end
 end
+
+# Driver Code
+# word = Word_Guesser.new('kaladesh', 'a')
+# p word.key_word
+# p word.guesses_allowed_counter
+# p word.store_letter
+# p word.hidden_word
+# p word.find_index
+# p word.show_letter
--- ruby/word-game/word_game_spec.rb
+++ ruby/word-game/word_game_spec.rb
@@ -1,21 +1,29 @@
 require_relative 'word_game'
 
 describe Word_Guesser do
-	let(:game) { Word_Guesser.new("kaladesh", "z") }
+	let(:game) { Word_Guesser.new("kaladesh", "a") }
 
 	it "stores the word User_1 inputted upon initialization" do
 		expect(game.key_word).to eq "kaladesh"
 	end
 
 	it "keeps track of how many guesses User_2 has left" do
-		expect(game.guesses_allowed_counter).to eq 8
+		expect(game.guesses_allowed_counter).to eq 16
 	end
 
 	it "stores the letter User_2 inputted" do
-		expect(game.store_letter).to eq "z"
+		expect(game.store_letter).to eq "a"
 	end
 
 	it "shows a hidden word with underscores each representing a letter in the word and spaces between each underscore" do
-		expect(game.hidden_word).to eq "_ _ _ _ _ _ _ _"
+		expect(game.hidden_word).to eq "________"
 	end
+
+	it "matches any instances of the letter within the inputted word" do
+		expect(game.find_index).to eq [1, 3]
+	end
+
+	# it "replaces the corresponding underscore(s) of the matched letters in the hidden word" do
+	# 	expect(game.show_letter).to eq "_a_a____"
+	# end
 end
