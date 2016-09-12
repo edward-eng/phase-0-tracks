0e5a103a08cb96e9151cf6192b43b4f5e03f1240
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-12 02:41:11 -0500

    Add user interface to class file.

--- ruby/word-game/word_guess.rb
+++ ruby/word-game/word_guess.rb
@@ -1,42 +1,37 @@
 # Pseudocode - secret word guessing game
 
-
 # The game will start out by greeting the players.
 # Then the game will ask player 1 for a secret word.
 # It’ll store the secret word.
 
-
 # Then the game will show player 2 a hidden word.
 # The hidden word will contain underscores each representing a letter of the secret word.
-
 
 # Then the game will tell player 2 how many guesses he or she has to win the game.
 # The number of guesses is equal to twice the length of the secret word.
 # Then the game will ask player 2 for a letter.
 # The game will store this letter.
 
-
 # Then the game will check to see if the letter is in the secret word.
 	# If the letter is in the word, replace the corresponding underscores with the letter.
 # The game will show player 2 an updated version of the hidden word.
 
-
 # The game will show player 2 the letters that he or she has guessed so far.
 # Then the game will ask player 2 for another letter.
-	# If player 2 enters a letter he or she has guessed before, prompt the player to enter another letter.
+	# If player 2 enters a letter he or she has guessed before, prompt the player to enter another letter. Then store that letter.
 	# Otherwise, store the letter.
 # The game will check to see if the letter is in the secret word.
 	# If the letter is in the word, update the latest version of the hidden word to replace all corresponding underscores.
 	# If there are no more letters to guess, print a message showing the player the correctly guessed word and a message saying ‘you win!’
 	# Otherwise, show the player an updated version of the hidden word and ask the player for another letter.
 
-
 # Repeat this process until the user wins the game or runs out of guesses.
 	# If the player runs out of guesses, print a message showing the last updated version of the hidden word and message saying ‘you lose!’
 
-
 # Exit the game
 
+
+# ========================================
 class Game
 	attr_reader :secret_word, :letter_entered, :secret_word_array
 	attr_accessor :hidden_word, :guesses, :guessed_letters
@@ -73,5 +68,70 @@ class Game
 
 	def lose
 		"Sorry, you don't have any guesses left. You lose. =("
+	end
+end
+
+
+# ========================================
+# USER INTERFACE
+
+puts "It's time to play 'Guess that Word'!!!"
+puts "Alright Player 1, please enter a secret word:"
+secret_word = gets.chomp
+word_guess = Game.new(secret_word)
+
+puts "Okay Player 2, here's your hidden word '#{word_guess.hidden_word}'."
+
+puts "You have #{word_guess.guesses} guesses to win the game. Good luck!"
+
+puts "Please enter a letter you'd like to guess:"
+letter = gets.chomp
+word_guess.store_letters(letter)
+word_guess.successful_letter(letter)
+
+until !word_guess.hidden_word.include?("_") || word_guess == 0
+	puts "Okay Player 2, here are the letters you've guessed so far...#{word_guess.guessed_letters}."
+	puts "'#{word_guess.hidden_word}'"
+	puts "You have #{word_guess.guesses - 1} guesses to win the game. Good luck!"
+	puts "Please enter another letter you'd like to guess."
+	next_letter = gets.chomp
+
+	if word_guess.guessed_letters.include?(next_letter)
+		puts "Sorry, you picked that letter before. Please enter another letter:"
+		another_letter = gets.chomp
+		word_guess.store_letters(another_letter)
+		word_guess.successful_letter(another_letter)
+		puts "'#{word_guess.hidden_word}'"
+		puts "Please enter another letter you'd like to guess."
+		if !word_guess.hidden_word.include?("_")
+			word_guess.win
+		else
+			puts "'#{word_guess.hidden_word}'"
+			puts "You have #{word_guess.guesses - 2} guesses to win the game. Good luck!"
+			puts "Please enter another letter you'd like to guess."
+			next_letter = gets.chomp
+			if word_guess.guesses == 0
+				puts "You guessed '#{word_guess.hidden_word}'"
+				word_guess.lose
+			end
+		end
+	else
+		word_guess.store_letters(next_letter)
+		word_guess.successful_letter(next_letter)
+		puts "'#{word_guess.hidden_word}'"
+		puts "You have #{word_guess.guesses} guesses to win the game. Good luck!"
+		puts "Please enter another letter you'd like to guess."
+		if !word_guess.hidden_word.include?("_")
+			word_guess.win
+		else
+			puts "'#{word_guess.hidden_word}'"
+			puts "You have #{word_guess.guesses - 2} guesses to win the game. Good luck!"
+			puts "Please enter another letter you'd like to guess."
+			next_letter = gets.chomp
+			if word_guess.guesses == 0
+				puts "You guessed '#{word_guess.hidden_word}'"
+				word_guess.lose
+			end
+		end
 	end
 end
