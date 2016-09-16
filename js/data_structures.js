5a624491c934bf0fac278cbba0e81f70b3798537
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-16 16:54:47 -0500

    Add car constructor function and driver code to test.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,27 +1,34 @@
-var colors = ["blue", "red", "green", "black"];
+// var colors = ["blue", "red", "green", "black"];
 
-var names = ["Penny", "Daisy", "Harry", "Potter"]
+// var names = ["Penny", "Daisy", "Harry", "Potter"]
 
-colors.push("brown");
-names.push("George");
+// colors.push("brown");
+// names.push("George");
 
-console.log(colors);
-console.log(names);
+// console.log(colors);
+// console.log(names);
 
-var horses = {};
+// var horses = {};
 
-for (var x = 0; x < names.length; x++) {
-  horses[names[x]] = colors[x];
+// for (var x = 0; x < names.length; x++) {
+//   horses[names[x]] = colors[x];
+// }
+
+// console.log(horses);
+
+function Car(make, model, color) {
+	this.make = make;
+	this.model = model;
+	this.color = color;
+
+	this.takeOff = function() {
+		console.log("0-60 in 3 seconds!");
+	};
 }
 
-console.log(horses);
-
-
-
-
-
-
-
+var newCar = new Car("Honda", "Civic", "white");
+console.log(newCar);
+newCar.takeOff();
 
 
 
