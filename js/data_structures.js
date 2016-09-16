64c8a0ebb1974060f55cbf6e933fb5961345844f
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-16 16:18:35 -0500

    Iterate through both arrays and store key-value pairs into an object for release 1.

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,7 +8,13 @@ names.push("George");
 console.log(colors);
 console.log(names);
 
+var horses = {};
 
+for (var x = 0; x < names.length; x++) {
+  horses[names[x]] = colors[x];
+}
+
+console.log(horses);
 
 
 
