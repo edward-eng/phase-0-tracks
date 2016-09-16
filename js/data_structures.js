585e635318ef5eaab1e07c42d8952282d3fee073
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-16 15:57:01 -0500

    Add one item each to both arrays. Print out arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,7 +2,11 @@ var colors = ["blue", "red", "green", "black"];
 
 var names = ["Penny", "Daisy", "Harry", "Potter"]
 
+colors.push("brown");
+names.push("George");
 
+console.log(colors);
+console.log(names);
 
 
 
