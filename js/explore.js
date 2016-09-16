f49a3ab7ccd9b3268612c91b283e31bdb9f465de
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-16 13:10:05 -0500

    Pseudocode reverseString function and add driver code to test.

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,25 @@
+// Declare a function that takes in a string as a parameter.
+// Split the characters of the string into individual strings and store them into an array.
+// Reverse the elements in the array.
+// Join the elements of the array together into a string and store the string in a variable.
+// Return the variable that contains the reversed string.
+
+function reverseString(string) {
+	var separate_letters = string.split('');
+	var reverse_letters = separate_letters.reverse();
+	var join_letters = reverse_letters.join('');
+	return join_letters
+}
+
+var reversed_string = reverseString('palindrome');
+console.log(reversed_string);
+
+
+
+
+// ========================================
+// OLD WORK
+
 // Write a function that takes a string as a parameter.
 // Store string in a variable.
 // Split the string into individual letters and store them in an array.
@@ -5,17 +27,17 @@
 // Put the elements of the array back together into a string.
 // Print the variable to the console if a conditional statement is true.
 
-function reverse_string(string){
-	var new_string = string;
-	var array = new_string.split("");
-	var reverse_array = array.reverse();
-	var result = reverse_array.join("");
+// function reverse_string(string){
+// 	var new_string = string;
+// 	var array = new_string.split("");
+// 	var reverse_array = array.reverse();
+// 	var result = reverse_array.join("");
 
-	if (1 == 1){
-	console.log(result);
-}
+// 	if (1 == 1){
+// 	console.log(result);
+// }
 
-}
+// }
 
-reverse_string("hello");
-reverse_string("Magic: The Gathering");
+// reverse_string("hello");
+// reverse_string("Magic: The Gathering");
