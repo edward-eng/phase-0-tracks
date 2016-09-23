6ad78fb79f040ae20eb99090cf3278eaed5fe643
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-23 18:12:13 -0500

    Update tables in database. Update search_results instance method.

--- databases/deals-finder/deals-finder.rb
+++ databases/deals-finder/deals-finder.rb
@@ -7,6 +7,7 @@ require 'sqlite3'
 # CREATE SQLite3 DATABASE
 
 db = SQLite3::Database.new("deals-finder.db")
+db.results_as_hash = true
 
 create_shoppers_table = <<-SQL
   CREATE TABLE IF NOT EXISTS shoppers(
@@ -59,16 +60,19 @@ class Deals_Finder
 	end
 
 	def create_business_owner(db, first_name, last_name, email, biz_name, biz_address, biz_phone, biz_fein, df_password)
-		db.execute("INSERT INTO business_owners (First_Name, Last_Name, Email_Address, Business_Name, Business_Address, Business_Phone, Business_FEIN, DF_Password) VALUES (?, ?, ?, ?, ?, ?, ?)", [first_name, last_name, email, biz_name, biz_address, biz_phone, biz_fein, df_password])
+		db.execute("INSERT INTO business_owners (First_Name, Last_Name, Email_Address, Business_Name, Business_Address, Business_Phone, Business_FEIN, DF_Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [first_name, last_name, email, biz_name, biz_address, biz_phone, biz_fein, df_password])
 	end
 
 	def create_deal(db, item_name, city, price, biz_owner_id)
-		db.execute("INSERT INTO deals (Item_Name, City, Price, Biz_Owner_ID,) VALUES (?, ?, ?, ?)", [first_name, last_name, email, item_name, city, price, biz_owner_id])
+		db.execute("INSERT INTO deals (Item_Name, City, Price, Biz_Owner_ID) VALUES (?, ?, ?, ?)", [item_name, city, price, biz_owner_id])
 	end
 
 	def search_results(db, city)
-		db.execute("SELECT * FROM deals (City) VALUES (?)", [city])
-	end
+    	results = db.execute("SELECT * FROM deals WHERE City=(?)", [city])
+    	results.each do | result |
+    		puts "#{result['ID']}|#{result['Item_Name']}|#{result['City']}|#{result['Price']}"
+    	end
+    end
 end
 
 # USER INTERFACE
