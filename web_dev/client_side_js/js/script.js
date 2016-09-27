60d1448e5332022a4f8a812768529d258d87cfa4
Vanessa Chiang <vcchiang@VanessaiangsAir.att.net>
2016-09-27 12:29:01 -0500

    Update script tag in index.html. Update script.js.

---
+++ web_dev/client_side_js/js/script.js
@@ -0,0 +1,12 @@
+console.log("test");
+
+var el = document.getElementsByTagName("p");
+
+firstPara = el[0];
+firstPara.style.fontFamily = "Impact,Charcoal,sans-serif";
+firstPara.style.fontSize = "20pt";
+
+var tacoPhoto = document.getElementsByTagName("img");
+
+tacoPic = tacoPhoto[0];
+tacoPic.style.border = "3px solid black";
